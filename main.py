e8f96a67b63e9138c682d11232a2eb99c44e414c
Luiserebii <luis@serebii.io>
2019-10-16 20:25:00 -0400

    Adding .gitignore, beginning minor calc project

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ Created on Wed Oct 16 17:32:11 2019
 """
 
 class Meme: 
-     def __init__(self, name, tag, value):
-         self.name = name;
-         self.tag = tag;
-         self.value = value;
-     def getName(self):
-         return self.name;
-     def getTag(self):
-         return self.tag;
-     def getValue(self):
-         return self.value;
-     def toString(self):
-         string = self.name + " " + self.tag + " " + str(self.value);
-         return string;
+    def __init__(self, name, tag, value):
+        self.name = name;
+        self.tag = tag;
+        self.value = value;
+    def getName(self):
+        return self.name;
+    def getTag(self):
+        return self.tag;
+    def getValue(self):
+        return self.value;
+    def toString(self):
+        string = self.name + " " + self.tag + " " + str(self.value);
+        return string;
     
     
 m = Meme("uguu", "gog", 100);
