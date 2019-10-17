de42e3d7cda8a21e56b21d5dd8700959e6820dbe
Luiserebii <luis@serebii.io>
2019-10-16 20:59:06 -0400

    Flesh out rest of options

--- calculator/main.py
+++ calculator/main.py
@@ -25,7 +25,7 @@ class Calculator:
         return a ** b;
 
 
-# Helper for printing
+# Helper functions, esp. for printing
 def list_to_string(l):
     s = ""
     for i in l:
@@ -42,6 +42,8 @@ def main():
     print("Currently supported functions are: \n" + list_to_string(FUNCTIONS_SUPPORTED))
     
     sel = input("Select an option: ")
+
+    # Apply logic based on selection
     if(sel == "add"):
         print("Specify two numbers, a and b")
         a = input("a: ")
@@ -49,6 +51,35 @@ def main():
         # Cast numbers to float via function, then add
         res = Calculator.add(float(a), float(b))
         print("Result: " + str(res))
+
+    elif(sel == "sub"):
+        print("Specify two numbers, a and b");
+        a = input("a: ")
+        b = input("b: ")
+        res = Calculator.sub(float(a), float(b))
+        print("Result " + str(res))
+
+    elif(sel == "mul"):
+        print("Specify two numbers, a and b");
+        a = input("a: ")
+        b = input("b: ")
+        res = Calculator.mul(float(a), float(b))
+        print("Result " + str(res))
+
+    elif(sel == "div"):
+        print("Specify two numbers, a and b");
+        a = input("a: ")
+        b = input("b: ")
+        res = Calculator.div(float(a), float(b))
+        print("Result " + str(res))
+
+    elif(sel == "power"):
+        print("Specify two numbers, a and b");
+        a = input("a: ")
+        b = input("b: ")
+        res = Calculator.power(float(a), float(b))
+        print("Result " + str(res))
+        
     else: 
         print("Function \"" + sel + "\" not implemented.")
 
