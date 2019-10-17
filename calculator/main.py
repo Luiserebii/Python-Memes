dfeee89fa6df2761c0e51b7f712e50ded27cf449
Luiserebii <luis@serebii.io>
2019-10-16 20:37:41 -0400

    Wrote out Calculator class

--- calculator/main.py
+++ calculator/main.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+class Calculator: 
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
+
+    @staticmethod
+    def mul(a, b):
+        return a - b
+
+    @staticmethod
+    def div(a, b):
+        return a / b
+
+    @staticmethod
+    def power(a, b):
+        return a ** b;
+
+print("Hello! Welcome~")
